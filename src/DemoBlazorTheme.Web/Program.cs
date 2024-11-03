c6dbff0cddfd39eba05bb57207e938314fb43070
Jesu Marquez <jesumarquez@gmail.com>
2024-11-03 13:08:03 -0300

    Mudblazor

--- src/DemoBlazorTheme.Web/Program.cs
+++ src/DemoBlazorTheme.Web/Program.cs
@@ -1,6 +1,5 @@
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
 using DemoBlazorTheme.Web.Data;
+using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddMudServices();
 
 var app = builder.Build();
 
